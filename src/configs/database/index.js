43166cc1b431ecfc28ad5890fe28fda7db0fea07
hainguyen <haigom00@gmail.com>
2021-10-03 21:19:16 +0700

    .

--- src/configs/database/index.js
+++ src/configs/database/index.js
@@ -4,9 +4,9 @@ initialize();
 
 async function initialize() {
   var con = await mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    host: process.env.DATABASE_HOST || "127.0.0.1",
+    user: process.env.USER || "root",
+    password: process.env.PASSWORD || "123456haie",
     });
 
   await con.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB}\`;`);
