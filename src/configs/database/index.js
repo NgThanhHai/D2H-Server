c820f8f2fa6533455f323f8a3f5642db57da0014
hainguyen <haigom00@gmail.com>
2021-12-04 20:20:57 +0700

    add database port

--- src/configs/database/index.js
+++ src/configs/database/index.js
@@ -5,6 +5,7 @@ initialize();
 async function initialize() {
   var con = await mysql.createConnection({
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     });
