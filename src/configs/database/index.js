f1f442287fc6363b6c8063df9d3a38d548052087
hainguyen <haigom00@gmail.com>
2021-12-06 14:24:01 +0700

    change await function

--- src/configs/database/index.js
+++ src/configs/database/index.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 initialize();
 
 async function initialize() {
-  var con = await mysql.createConnection({
+  var con = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     });
 
-  await con.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB}\`;`);
+  con.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB}\`;`);
 } 
