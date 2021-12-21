1b6d97600f32fe37cbc16a7e5e45ea936e1e22a0
hainguyen <haigom00@gmail.com>
2021-12-22 00:23:01 +0700

    test fix app crash

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ if (dotenv.error) {
 const app = express();
 const port = process.env.PORT || 8000
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
