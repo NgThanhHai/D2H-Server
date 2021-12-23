064872625f8ef615229d94d750caa066c6e333dd
hainguyen <haigom00@gmail.com>
2021-12-23 13:43:19 +0700

    fix cors

--- server.js
+++ server.js
@@ -17,12 +17,11 @@ if (dotenv.error) {
 const app = express();
 const port = process.env.PORT || 8000
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
 
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
@@ -37,7 +36,14 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'app/public')));
 
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 app.use('/api', indexRouter);
 app.listen(port, () => console.log(`Server listen on port ${port}!`));
 
