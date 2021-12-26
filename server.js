6df31e82c147695d3833791c24635a5dd458bd1d
hainguyen <haigom00@gmail.com>
2021-12-26 22:28:38 +0700

    fix cors

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ const port = process.env.PORT || 8000
     
 // }
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -38,7 +38,7 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'app/public')));
 
 
-// app.use(cors(corsOptions));
+app.use(cors());
 
 
 app.listen(port, () => console.log(`Server listen on port ${port}!`));
