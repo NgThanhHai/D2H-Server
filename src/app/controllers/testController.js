40030c898f79773e0992183d7f8b0afe2c930f06
hainguyen <haigom00@gmail.com>
2022-02-21 17:42:33 +0700

    fix wrong format

--- src/app/controllers/testController.js
+++ src/app/controllers/testController.js
@@ -448,7 +448,7 @@ exports.getAllTest = [auth, function (req, res) {
                                 return apiResponse.successResponseWithPagingData(res, "Success", testCollection, getPagingData(page), testCollection.length)
 
                             } else {
-                                return apiResponse.successResponse(res, "Test not existed")
+                                return apiResponse.successResponseWithData(res, "Test not existed", [])
                             }
                         }).catch(err => {
 
